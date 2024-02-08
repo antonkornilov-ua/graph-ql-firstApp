32dc96ba011e828df3478481e4d6f980f6f7d879
antonkornilov-ua <anton.kornilov@hotmail.com>
2024-02-08 14:28:36 +0200

    Add fake db, create query for user and users, add mutation for user

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import { users } from './fakedb.js';
 import { createHandler } from 'graphql-http/lib/use/express';
 
 const app = express();
 
-import { GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql';
+import { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLList } from 'graphql';
 
 /**
  * Construct a GraphQL schema and define the necessary resolvers.
@@ -16,9 +17,34 @@ const schema = new GraphQLSchema({
     query: new GraphQLObjectType({
         name: 'Query',
         fields: {
-            hello: {
+            users: {
+                type: new GraphQLList(GraphQLString),
+                resolve: () => users,
+            },
+            user: {
                 type: GraphQLString,
-                resolve: () => 'world',
+                args: {
+                    name: { type: GraphQLString },
+                },
+                resolve: (parent, args) => users.find((item) => item === args.name),
+            },
+        },
+    }),
+    mutation: new GraphQLObjectType({
+        name: 'Mutation',
+        fields: {
+            updateUser: {
+                type: GraphQLString,
+                args: {
+                    oldName: { type: GraphQLString },
+                    newName: { type: GraphQLString },
+                },
+                resolve: (parent, args) => {
+                    const index = users.indexOf(args.oldName);
+                    if (index === -1) return null;
+                    users[ index ] = args.newName;
+                    return args.newName
+                }
             },
         },
     }),
