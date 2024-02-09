b2cdf21834f094013b301c207de331c8b5dcd6b8
antonkornilov-ua <anton.kornilov@hotmail.com>
2024-02-09 13:18:10 +0200

    issues solved, more complicated structure

--- schema/post/type.js
+++ schema/post/type.js
@@ -1,14 +1,36 @@
-import { GraphQLObjectType, GraphQLString, GraphQLNonNull, GraphQLID, GraphQLInputObjectType } from 'graphql';
-
+import {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLNonNull,
+    GraphQLID,
+    GraphQLInputObjectType,
+    GraphQLList,
+} from 'graphql';
+import { UserType } from '../user/type.js';
+import { users, likes } from '../../fakedb.js';
 
 const PostType = new GraphQLObjectType({
     name: 'PostType',
-    fields: {
+    fields: () => ({
         id: { type: new GraphQLNonNull(GraphQLID) },
         userId: { type: new GraphQLNonNull(GraphQLID) },
+        user: {
+            type: new GraphQLNonNull(UserType),
+            resolve: (parent) => users.find((user) => user.id === parent.userId),
+        },
+        likedBy: {
+            type: new GraphQLList(UserType),
+            resolve: (parent) =>
+                users.filter((user) =>
+                    likes
+                        .filter((like) => like.postId === parent.id)
+                        .map((like) => like.userId)
+                        .includes(user.id)
+                ),
+        },
         title: { type: new GraphQLNonNull(GraphQLString) },
         body: { type: new GraphQLNonNull(GraphQLString) },
-    },
+    }),
 });
 const PostCreateType = new GraphQLInputObjectType({
     name: 'PostCreateType',
--- schema/user/type.js
+++ schema/user/type.js
@@ -1,11 +1,35 @@
-import { GraphQLObjectType, GraphQLString, GraphQLNonNull, GraphQLID, GraphQLInputObjectType } from 'graphql';
+import {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLNonNull,
+    GraphQLID,
+    GraphQLInputObjectType,
+    GraphQLList,
+} from 'graphql';
+import { PostType } from '../post/type.js';
+import { posts, likes } from '../../fakedb.js';
+
 const UserType = new GraphQLObjectType({
     name: 'UserType',
-    fields: {
+    fields: () => ({
         id: { type: new GraphQLNonNull(GraphQLID) },
         name: { type: new GraphQLNonNull(GraphQLString) },
         email: { type: new GraphQLNonNull(GraphQLString) },
-    },
+        postCreated: {
+            type: new GraphQLList(PostType),
+            resolve: (parent) => posts.filter((post) => post.userId === parent.id),
+        },
+        postLiked: {
+            type: new GraphQLList(PostType),
+            resolve: (parent) =>
+                posts.filter((post) =>
+                    likes
+                        .filter((like) => like.userId === parent.id)
+                        .map((like) => like.postId)
+                        .includes(post.id)
+                ),
+        },
+    }),
 });
 
 const UserInputType = new GraphQLInputObjectType({
